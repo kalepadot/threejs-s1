41856b8b05310c8bdc6b28b25514efdd7d3455b5
Jeremy Kale Padot <jeremy@padot.us>
2023-02-12 18:08:24 -0700

    add orbital controls, controle args and auto rotate

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 //Import Three!
 import * as THREE from "three";
 import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 //First set up a scene, imagine this like a movie set. you have your cameras, lights, background, and actors
 
 //Scene
@@ -69,6 +71,14 @@ renderer.render(scene, camera);
 
 // lets make it fit the whole screen, jump up to 'sizes' ** you can now change you aspect ratio previously 800 / 600 with this variable of width and height *window
 
+const controls = new OrbitControls(camera, canvas);
+//add some damping - better movement when dragging responsive to how fast or slow / physics
+controls.enableDamping = true;
+//this is a website so lets disable zoom and such
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
 //Resize
 window.addEventListener("resize", () => {
   //update sizes for responsive behavior
@@ -82,7 +92,11 @@ window.addEventListener("resize", () => {
 });
 //we need this camera constantly re-rendering when we resize to avoid smashing our object, lets make a render loop
 const loop = () => {
+  // mesh.position.x += 0.2; you could add an animation here if you'd like .position .rotation light etc *going to use gsap
+  controls.update(); //keeps moving when you let go
   renderer.render(scene, camera);
   window.requestAnimationFrame(loop);
 };
 loop();
+
+//now lets add some orbital controls *import from THREE, see Controls above
